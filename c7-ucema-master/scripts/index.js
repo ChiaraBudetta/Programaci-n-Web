caea941181195125338eecd067f71f8a348961ea
Chiara B <budettachiara@gmail.com>
2023-10-11 16:28:43 -0300

    cambios para entrega final

---
+++ c7-ucema-master/scripts/index.js
@@ -0,0 +1,18 @@
+const menuButton = document.getElementById("menu")
+const nav = document.getElementById("nav")
+const closeButton = document.getElementById("close")
+
+
+menuButton.addEventListener("click", function(){
+    nav.style.display = "inline-block"
+    closeButton.style.display ="inline-block"
+    menuButton.style.display = "none"
+})
+
+
+menuButton.addEventListener("click", function(){
+    nav.style.display = ""
+    menuButton.style.display =""
+    closeButton.style.display = ""
+})
+
